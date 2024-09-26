80f517b639d50e2fe8d6273c37cfbc0de74c8b6a
Jingxiang Mo <jingxiangmo@gmail.com>
2024-09-26 17:15:40 -0400

    async loops

--- runtime/src/main.rs
+++ runtime/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
+use tokio;
+use std::time::Duration;
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+
+    let fbk = tokio::spawn(async_fbk());
+    let set = tokio::spawn(async_set());
+
+    tokio::join!(fbk, set);
+}
+
+async fn async_fbk() {
+    let interval = Duration::from_micros(10000); // 100 Hz
+    let mut interval = tokio::time::interval(interval);
+    loop {
+        interval.tick().await;
+        println!("Running feedback...");
+    }
+}
+
+async fn async_set() {
+    let interval = Duration::from_micros(20000); // 50 Hz 
+    let mut interval = tokio::time::interval(interval);
+    loop {
+        interval.tick().await;
+        println!("Running set...");
+    }
 }
