4009546d583fcdb0751176228c9951f294bc061a
Jingxiang Mo <jingxiangmo@gmail.com>
2024-09-26 17:43:20 -0400

    streaming tests

--- runtime/src/main.rs
+++ runtime/src/main.rs
@@ -1,5 +1,23 @@
 use tokio;
 use std::time::Duration;
+use rand::Rng;
+
+// tests
+fn generate_random_joint_positions() -> Vec<f32> {
+    let mut rng = rand::thread_rng();
+    (0..16).map(|_| rng.gen_range(0.0..std::f32::consts::TAU)).collect()
+}
+
+async fn stream_test_joint_positions() -> Vec<f32> {
+    let interval = Duration::from_millis(10); // 100 Hz
+    let mut interval = tokio::time::interval(interval);
+    loop {
+        interval.tick().await;
+        let positions = generate_random_joint_positions();
+        println!("Joint positions: {:?}", positions);
+
+    }
+}
 
 
 // joint_states -> getting joint position feedback (feedback position)
@@ -25,20 +43,24 @@ async fn joint_commands() {
 
 
 // control loop
-fn control_loop() {
+async fn controller() {
 
+    // stream out fake test data
+    let test = tokio::spawn(stream_test_joint_positions());
+
+
+    // joint states
     let joint_states = tokio::spawn(joint_states());
     let joint_commands = tokio::spawn(joint_commands());
 
-    tokio::join!(joint_states, joint_commands);
+    tokio::join!(test, joint_states, joint_commands);
 }
-
 
 // main control loop
 #[tokio::main]
 async fn main() {
     println!("Runtime loop experiments");
 
-    control_loop();
+    controller().await;
 }
 
