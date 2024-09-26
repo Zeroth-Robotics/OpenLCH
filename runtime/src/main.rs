2c584472b3fc20b5222de3651e4707d21d29b75c
Jingxiang Mo <jingxiangmo@gmail.com>
2024-09-26 17:22:04 -0400

    joints states/commands

--- runtime/src/main.rs
+++ runtime/src/main.rs
@@ -1,17 +1,9 @@
 use tokio;
 use std::time::Duration;
 
-#[tokio::main]
-async fn main() {
-    println!("Hello, world!");
 
-    let fbk = tokio::spawn(async_fbk());
-    let set = tokio::spawn(async_set());
-
-    tokio::join!(fbk, set);
-}
-
-async fn async_fbk() {
+// joint_states -> getting joint position feedback (feedback position)
+async fn joint_states() {
     let interval = Duration::from_micros(10000); // 100 Hz
     let mut interval = tokio::time::interval(interval);
     loop {
@@ -20,7 +12,9 @@ async fn async_fbk() {
     }
 }
 
-async fn async_set() {
+
+// joint_commands -> sending joint position commands (desired position)
+async fn joint_commands() {
     let interval = Duration::from_micros(20000); // 50 Hz 
     let mut interval = tokio::time::interval(interval);
     loop {
@@ -28,3 +22,23 @@ async fn async_set() {
         println!("Running set...");
     }
 }
+
+
+// control loop
+fn control_loop() {
+
+    let joint_states = tokio::spawn(joint_states());
+    let joint_commands = tokio::spawn(joint_commands());
+
+    tokio::join!(joint_states, joint_commands);
+}
+
+
+// main control loop
+#[tokio::main]
+async fn main() {
+    println!("Runtime loop experiments");
+
+    control_loop();
+}
+
