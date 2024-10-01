acb0584cb563636fe792eafc3e8ad82f97287ddc
Henri Lemoine <henri123lemoine@gmail.com>
2024-10-01 16:27:11 -0400

    Set up ML and PID controllers in controller.rs

--- runtime/src/controller.rs
+++ runtime/src/controller.rs
@@ -1,40 +1,65 @@
-use crate::model::Controller;
+use crate::model::Model;
 use crate::robot::Robot;
 use anyhow::Result;
-use onnxruntime::ndarray::Array1;
+use std::sync::Arc;
 use std::time::Instant;
-use tokio::time::{sleep, Duration};
 
-pub struct StandingControllerPID {
-    robot: Robot,
-    controller: Controller,
+pub trait Controller {
+    fn compute_action(&self, state: &[f32]) -> Result<Vec<f32>>;
 }
 
-impl StandingControllerPID {
-    pub fn new(robot: Robot, controller: Controller) -> Self {
+pub struct PIDController {
+    // PID parameters
+}
+
+impl Controller for PIDController {
+    fn compute_action(&self, state: &[f32]) -> Result<Vec<f32>> {
+        // PID control logic
+        println!("Computing action with PID controller");
+        Ok(vec![0.0; state.len()])
+    }
+}
+
+pub struct MLController {
+    model: Model,
+}
+
+impl MLController {
+    pub fn new(model: Model) -> Self {
+        Self { model }
+    }
+}
+
+impl Controller for MLController {
+    fn compute_action(&self, state: &[f32]) -> Result<Vec<f32>> {
+        // ML control logic
+        println!("Computing action with ML controller");
+        self.model.infer(state)
+    }
+}
+
+pub struct StandingController {
+    robot: Robot,
+    controller: Arc<dyn Controller + Send + Sync>,
+}
+
+impl StandingController {
+    pub fn new(robot: Robot, controller: Arc<dyn Controller + Send + Sync>) -> Self {
         Self { robot, controller }
     }
 
-    pub async fn get_state(&self) -> Result<Array1<f32>> {
+    pub async fn get_state(&self) -> Result<Vec<f32>> {
         // ### === TODO: DENYS === ###
         // let state = self.robot.joint_states().await;
         // let imu = self.robot.imu_state().await;
         // let action = self.model.infer(state, imu).await?;
-        Ok(Array1::zeros(10)) // Placeholder
+        Ok(vec![0.0; 10])
     }
 
-    // ### === TODO: DENYS === ###
-    pub async fn get_feedback(&self) -> Result<()> {
+    pub async fn send_command(&self, command: &[f32]) -> Result<()> {
+        // ### === TODO: DENYS === ###
         let time = Instant::now();
-        println!("feedback");
-        println!("Start time: {:?}", time);
-        Ok(())
-    }
-
-    // ### === TODO: DENYS === ###
-    pub async fn send_command(&self) -> Result<()> {
-        let time = Instant::now();
-        println!("command");
+        println!("Sending command: {:?}", command);
         println!("Start time: {:?}", time);
         Ok(())
     }
@@ -44,7 +69,7 @@ impl StandingControllerPID {
         let mut commands = Vec::new();
 
         for (joint_name, &position) in desired_positions {
-            if let Some(joint_config) = self.find_joint_config(joint_name) {
+            if let Some(joint_config) = self.robot.find_joint_config(joint_name) {
                 commands.push((joint_config.id, position));
             }
         }
@@ -56,62 +81,18 @@ impl StandingControllerPID {
         Ok(())
     }
 
-    fn find_joint_config(&self, joint_name: &str) -> Option<&crate::robot::JointConfig> {
-        for limb in [&self.robot.config.legs, &self.robot.config.arms] {
-            for side_joints in limb.values() {
-                if let Some(joint_config) = side_joints.get(joint_name) {
-                    return Some(joint_config);
-                }
-            }
-        }
-        None
-    }
-
     pub async fn run(&mut self) -> Result<()> {
-        // get joint states
-        // get imu state
-        // send command
+        println!("Starting StandingController");
+        for i in 0..5 {
+            // Run for 5 iterations only
+            println!("Controller iteration {}", i);
+            let state = self.get_state().await?;
+            let command = self.controller.compute_action(&state)?;
+            self.send_command(&command).await?;
 
-        println!("Starting StandingControllerPID");
-        loop {
-            let feedback_future = self.get_feedback();
-            let command_future = self.send_command();
-
-            tokio::try_join!(feedback_future, command_future)?;
-
-            sleep(Duration::from_millis(100)).await;
+            tokio::time::sleep(std::time::Duration::from_millis(100)).await;
         }
+        println!("StandingController finished");
+        Ok(())
     }
 }
-
-// pub struct StandingControllerPPO {
-//     model: OnnxInfer,
-//     robot: Robot,
-//     config: Config,
-// }
-
-// impl StandingControllerPPO {
-//     pub fn new(model: OnnxInfer, robot: Robot, config: Config) -> Self {
-//         Self {
-//             model,
-//             robot,
-//             config,
-//         }
-//     }
-
-//     pub fn get_state(&self) -> Result<()> {
-//         let state = self.robot.joint_states();
-//         let imu = self.robot.imu_state();
-//         let action = self.model.infer(state, imu)?;
-//         Ok(())
-//     }
-
-//     pub fn send_commands(&self) -> Result<()> {
-//         Ok(())
-//     }
-
-//     pub fn run(&self) -> Result<()> {
-//         Ok(())
-//     }
-
-// }
