c33aa7e2723ac450862e0c539368b374371ee0bd
Henri Lemoine <henri123lemoine@gmail.com>
2024-10-01 01:41:15 -0400

    Moved stompymicro config to config/ file

--- runtime/src/main.rs
+++ runtime/src/main.rs
@@ -8,7 +8,7 @@ use std::path::PathBuf;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let config_path = PathBuf::from("config.toml");
+    let config_path = PathBuf::from("config/stompymicro.toml");
     let robot = Robot::new(config_path)?;
 
     println!("Robot initialized. Printing configuration:");
