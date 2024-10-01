6f0af13ebe8dbdbd4569bba951787d244ed34e78
Henri Lemoine <henri123lemoine@gmail.com>
2024-10-01 16:27:42 -0400

    Added debugging and controller selection in main

--- runtime/src/main.rs
+++ runtime/src/main.rs
@@ -1,31 +1,49 @@
+// src/main.rs
+
 mod HAL;
 mod controller;
 mod model;
 mod robot;
 
-use crate::controller::*;
-use crate::model::Controller;
-use anyhow::Result;
-use robot::Robot;
+use anyhow::{Context, Result};
+use candle_core::Device;
 use std::path::PathBuf;
+use std::sync::Arc;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     println!("Starting robot initialization");
+
     let config_path = PathBuf::from("config/stompymicro.toml");
-    let robot = Robot::new(config_path)?;
+    println!("Loading config from: {:?}", config_path);
+
+    let robot = robot::Robot::new(config_path).context("Failed to initialize robot")?;
 
     println!("Robot initialized. Printing configuration:");
     robot.print_config();
 
-    // PPO
-    let model_path = PathBuf::from("path/model.onnx");
-    let controller = Controller::new_ppo(model_path)?;
+    println!("Initializing controller"); // Onnx or PID
 
-    // Handwritten
-    // let controller = Controller::new_handwritten();
+    // Onnx
+    let model_path = PathBuf::from("path/to/model.onnx");
+    let device = Device::Cpu; // or Device::Cuda(0) for GPU
+    let model = model::Model::new(model_path, &device)?;
+    let controller: Arc<dyn controller::Controller + Send + Sync> =
+        Arc::new(controller::MLController::new(model));
 
-    println!("Creating StandingControllerPID");
-    let mut standing_controller = StandingControllerPID::new(robot, controller);
-    standing_controller.run().await
+    // PID
+    // let controller: Arc<dyn controller::Controller + Send + Sync> =
+    //     Arc::new(controller::PIDController {});
+
+    println!("Creating StandingController");
+    let mut standing_controller = controller::StandingController::new(robot, controller);
+
+    println!("Starting controller");
+    standing_controller
+        .run()
+        .await
+        .context("Controller run failed")?;
+
+    println!("Controller finished running");
+    Ok(())
 }
