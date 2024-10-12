2589c254696a553fcafce97a73aebf4769972831
JX <jingxiangmo@gmail.com>
2024-10-12 13:43:58 -0700

    added models

--- runtime/src/main.rs
+++ runtime/src/main.rs
@@ -2,22 +2,30 @@ mod controller;
 mod model;
 mod robot;
 use anyhow::Result;
+use rand::Rng;
+use half::f16;
+use std::path::PathBuf;
+
 
 fn main() -> Result<()> {
-    // println!("Starting robot initialization");
 
-    // let config_path = PathBuf::from("config/stompymicro.toml");
-    // println!("Loading config from: {:?}", config_path);
+    println!("Initializing robot...");
 
     // let robot = robot::Robot::new(config_path).context("Failed to initialize robot")?;
-
     // println!("Robot initialized. Printing configuration:");
-
     // robot.print_config();
 
-    // println!("Initializing controller...");
+    println!("Robot initialized.");
 
-    controller::run()?;
+    println!("Loading model...");
+
+    let model_path = PathBuf::from("/root/standing.cvi");
+    let model = model::Model::new(model_path)?;
+
+    println!("Model loaded.");
+
+    println!("Controller started...");
+    controller::run(); // TODO, pass model or other controller parameters
     
     Ok(())
 }
