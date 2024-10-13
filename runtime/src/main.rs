56633c6c7fabc4c54feac22a4078d78caeb1d516
JX <jingxiangmo@gmail.com>
2024-10-12 17:41:33 -0700

    runtime model inference

--- runtime/src/main.rs
+++ runtime/src/main.rs
@@ -18,14 +18,28 @@ fn main() -> Result<()> {
     println!("Robot initialized.");
 
     println!("Loading model...");
-
-    let model_path = PathBuf::from("/root/standing.cvi");
+    let model_path = PathBuf::from("/root/models/ppo_walking.cvimodel"); // PATH IN MILK-V
     let model = model::Model::new(model_path)?;
 
     println!("Model loaded.");
 
+    // generate 615 random float32 values with reduced precision
+    let mut rng = rand::thread_rng();
+    let input: Vec<f32> = (0..615).map(|_| {
+        let f32_value: f32 = rng.gen();
+        // Convert to f16 and back to f32 to simulate float16 precision
+        f16::from_f32(f32_value).to_f32()
+    }).collect();
+
+     // run inference
+     let output = model.infer(&input)?;
+
+     println!("Inference completed. Output size: {}", output.len());
+     println!("First 5 output values: {:?}", &output[..5.min(output.len())]);
+
+
     println!("Controller started...");
-    controller::run(); // TODO, pass model or other controller parameters
+    // controller::run(); // TODO pass model or other controller parameters
     
     Ok(())
 }
