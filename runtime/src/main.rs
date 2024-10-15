8e4c00f9309ade02da69747196c7e1e961853ca0
JX <jingxiangmo@gmail.com>
2024-10-15 12:35:49 -0700

    fix initalization in main.rs

--- runtime/src/main.rs
+++ runtime/src/main.rs
@@ -1,23 +1,26 @@
 mod controller;
 mod model;
+mod robot;
 
+use crate::robot::Robot;
+use crate::model::Model;
 use anyhow::{Context, Result};
-use runtime::hal;
+// use runtime::hal; 
 use std::path::PathBuf;
-use std::env;
-use runtime::model::Model;
 use std::sync::Arc;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    // initialize robot
-    let robot = robot::Robot::new().context("Failed to initialize robot")?;
-
     // load model
     let model_path = PathBuf::from("/root/models/ppo_walking.cvimodel"); // PATH IN MILK-V
     let model = Model::new(model_path).context("Failed to load model")?;
-    let model_arc = Arc::new(model);
+    let model = Arc::new(model);
+
+    // initialize robot
+    let robot = Robot::new()?;
+    let robot = Arc::new(robot);
+    robot.initialize().await?;
 
     // run controller
-    controller::run_controller(model_arc, robot).await.context("Controller encountered an error")
+    controller::run(model, robot).await.context("Controller encountered an error")
 }
