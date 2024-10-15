dab27d3b17efd6728980fa92df8d06e6dee1354e
Henri Lemoine <henri123lemoine@gmail.com>
2024-10-15 13:40:15 -0400

    Minor refactor: fixed warnings and errors in controller

--- runtime/src/controller.rs
+++ runtime/src/controller.rs
@@ -1,20 +1,16 @@
+use crate::model::Model;
 use anyhow::Result;
 use runtime::hal::{Servo, ServoMultipleWriteCommand, MAX_SERVOS};
-use tokio::time::{sleep, interval, Duration};
 use std::sync::Arc;
-use tokio::sync::Mutex;
-use runtime::hal::Model;
-use ndarray::Array1;
-
-pub struct Robot{
-
+use tokio::time::interval;
+use tokio::time::Duration;
+pub struct Robot {
     servo: Servo,
 }
 
-impl Robot{
+impl Robot {
     pub fn new() -> Result<Self> {
         let servo = Servo::new()?;
-
         Ok(Self { servo })
     }
 
@@ -26,10 +22,11 @@ impl Robot{
 
             // get joint states
             let current_joint_states = self.get_joint_states().await?;
-            
+
             // get desired joint positions (inferenced from model)
-            let desired_joint_positions = self.model_inference(&model, &current_joint_states).await?;
-            
+            let desired_joint_positions =
+                self.model_inference(&model, &current_joint_states).await?;
+
             // send joint commands
             self.send_joint_commands(&desired_joint_positions).await?;
         }
@@ -37,7 +34,9 @@ impl Robot{
 
     async fn get_joint_states(&self) -> Result<[f32; 16]> {
         let servo_data = self.servo.read_continuous()?;
-        let joint_states: [f32; 16] = servo_data.servo.iter()
+        let joint_states: [f32; 16] = servo_data
+            .servo
+            .iter()
             .take(16)
             .map(|s| s.target_location as f32)
             .collect::<Vec<f32>>()
@@ -47,7 +46,6 @@ impl Robot{
     }
 
     async fn model_inference(&self, model: &Model, joint_states: &[f32; 16]) -> Result<[f32; 16]> {
-        //  
         // x_vel: Array1<f32>,
         // y_vel: Array1<f32>,
         // rot: Array1<f32>,
@@ -59,18 +57,17 @@ impl Robot{
         // imu_euler_xyz: Array1<f32>,
         // buffer: Array1<f32>,
 
-
         let model_output = model.infer(joint_states)?;
 
         // TODO: Implement proper conversion from model output to desired joint positions
-        let desired_joint_positions: [f32; 16] = model_output.iter()
+        let desired_joint_positions: [f32; 16] = model_output
+            .iter()
             .take(16)
-            .map(|&x| x)
+            .copied()
             .collect::<Vec<f32>>()
             .try_into()
             .unwrap_or([0.0; 16]);
-
-        Ok(desired_joint_positions)   
+        Ok(desired_joint_positions)
     }
 
     async fn send_joint_commands(&self, positions: &[f32; 16]) -> Result<()> {
@@ -82,27 +79,21 @@ impl Robot{
             only_write_positions: 0,
         };
 
-        for i in 0..16 {
+        for (i, &position) in positions.iter().enumerate().take(16) {
             cmd.ids[i] = (i + 1) as u8;
-            cmd.positions[i] = positions[i] as i16;
+            cmd.positions[i] = position as i16;
             cmd.times[i] = 20;
         }
 
         self.servo.write_multiple(&cmd)?;
 
         println!("Command sent to move all servos to position {} with time {} ms and speed {}, send_only_positions: {}", positions[0] as i16, 20, 0, 0);
+
+        Ok(())
     }
 }
 
-#[tokio::main]
-pub async fn main(model: Arc<Model>, robot: Arc<Robot>) -> Result<()> {
-
-    robot.servo.enable_readout()?;  
-
-    robot.run(model).await?;
-
-    Ok(())
+pub async fn run(model: Arc<Model>, robot: Arc<Robot>) -> Result<()> {
+    robot.servo.enable_readout()?;
+    robot.run(model).await
 }
-
-
-
--- runtime/src/main.rs
+++ runtime/src/main.rs
@@ -1,22 +1,21 @@
 mod controller;
 mod model;
 
-use anyhow::{Context, Result};
+use anyhow::Result;
+use controller::Robot;
+use model::Model;
 use std::path::PathBuf;
-use std::env;
-use runtime::model::Model;
 use std::sync::Arc;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     // initialize robot
-    let robot = robot::Robot::new().context("Failed to initialize robot")?;
+    let robot = Arc::new(Robot::new()?);
 
     // load model
     let model_path = PathBuf::from("/root/models/ppo_walking.cvimodel"); // PATH IN MILK-V
-    let model = Model::new(model_path).context("Failed to load model")?;
-    let model_arc = Arc::new(model);
+    let model = Arc::new(Model::new(model_path)?);
 
     // run controller
-    controller::main(model_arc, robot).await.context("Controller encountered an error")
+    controller::run(model, robot).await
 }
