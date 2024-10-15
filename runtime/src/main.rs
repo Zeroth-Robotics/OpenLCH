82af024460cf7b29ebd388ab6b4c35094fee3767
Henri Lemoine <henri123lemoine@gmail.com>
2024-10-15 13:55:26 -0400

    Added back context

--- runtime/src/main.rs
+++ runtime/src/main.rs
@@ -1,7 +1,7 @@
 mod controller;
 mod model;
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 use controller::Robot;
 use model::Model;
 use std::path::PathBuf;
@@ -10,12 +10,14 @@ use std::sync::Arc;
 #[tokio::main]
 async fn main() -> Result<()> {
     // initialize robot
-    let robot = Arc::new(Robot::new()?);
+    let robot = Arc::new(Robot::new().context("Failed to initialize robot")?);
 
     // load model
     let model_path = PathBuf::from("/root/models/ppo_walking.cvimodel"); // PATH IN MILK-V
-    let model = Arc::new(Model::new(model_path)?);
+    let model = Arc::new(Model::new(model_path).context("Failed to load model")?);
 
     // run controller
-    controller::run(model, robot).await
+    controller::run(model, robot)
+        .await
+        .context("Controller encountered an error")
 }
