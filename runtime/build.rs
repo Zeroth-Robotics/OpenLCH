b187fba97e3fc1d21f1e295d2512b7c348df63d6
Denys Bezmenov <me@hatomist.pw>
2024-10-15 20:13:39 -0700

    sts_server WIP

--- runtime/build.rs
+++ runtime/build.rs
@@ -3,6 +3,7 @@ fn main() {
     {
         println!("cargo:rustc-link-search=native=./firmware/cviwrapper");
         println!("cargo:rustc-link-lib=dylib=cviwrapper");
+        tonic_build::compile_protos("proto/hal.proto")?;
     }
     
     println!("cargo:rerun-if-changed=build.rs");
