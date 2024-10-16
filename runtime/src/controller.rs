bba498f39c1f4d9a7e41964e317b4730141105ae
JX <jingxiangmo@gmail.com>
2024-10-15 17:09:13 -0700

    oof

--- runtime/src/controller.rs
+++ runtime/src/controller.rs
@@ -1,17 +1,17 @@
 use anyhow::Result;
 use tokio::time::{interval, Duration};
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use crate::model::Model;
 use crate::robot::Robot;
 
-pub async fn run(model: Arc<Model>, robot: Arc<Robot>) -> Result<()> {
-    let mut control_interval = interval(Duration::from_millis(20)); // 50HZ, for the sake of example
+// pub async fn run(model: Arc<Model>, robot: Arc<Mutex<Robot>>) -> Result<()> {
+//     let mut control_interval = interval(Duration::from_millis(20)); // 50HZ, for the sake of example
 
-    loop {
-        control_interval.tick().await;
+//     loop {
+//         control_interval.tick().await;
 
-        let current_joint_states = robot.get_joint_states().await?;
-        let desired_joint_positions = robot.model_inference(&model).await?;
-        robot.send_joint_commands(&desired_joint_positions).await?;
-    }
-}
+//         let current_joint_states = robot.lock().unwrap().get_joint_states().await?;
+//         let desired_joint_positions = robot.lock().unwrap().model_inference(&model).await?;
+//         robot.lock().unwrap().send_joint_commands(&desired_joint_positions).await?;
+//     }
+// }
